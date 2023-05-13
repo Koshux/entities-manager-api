3e348af0fb486248711c669f1de28bb429cafe39
Koshux <lanzonprojects@gmail.com>
2023-05-13 17:08:19 +0200

    Created new API
    Re-created the API since the previous project was not using the latest versions.

---
+++ api-new/contracts/env.ts
@@ -0,0 +1,35 @@
+/**
+ * Contract source: https://git.io/JTm6U
+ *
+ * Feel free to let us know via PR, if you find something broken in this contract
+ * file.
+ */
+import Env from '@ioc:Adonis/Core/Env'
+
+declare module '@ioc:Adonis/Core/Env' {
+  /*
+  |--------------------------------------------------------------------------
+  | Getting types for validated environment variables
+  |--------------------------------------------------------------------------
+  |
+  | The `default` export from the "../env.ts" file exports types for the
+  | validated environment variables. Here we merge them with the `EnvTypes`
+  | interface so that you can enjoy intellisense when using the "Env"
+  | module.
+  |
+  */
+
+  type CustomTypes = typeof import('../env').default
+  interface EnvTypes extends CustomTypes {
+
+  }
+}
+
+export default Env.rules({
+  DB_CONNECTION: Env.schema.string(),
+  PG_HOST: Env.schema.string({ format: 'host' }),
+  PG_PORT: Env.schema.number(),
+  PG_USER: Env.schema.string(),
+  PG_PASSWORD: Env.schema.string.optional(),
+  PG_DB_NAME: Env.schema.string(),
+})
---
+++ api-new/database/factories/index.ts
@@ -0,0 +1,29 @@
+// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Env from '@ioc:Adonis/Core/Env'
+import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+
+const databaseConfig: DatabaseConfig = {
+  // Default connection
+  connection: Env.get('DB_CONNECTION'),
+
+  // List of available connections
+  connections: {
+    pg: {
+      client: 'pg',
+      connection: {
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    },
+  }
+}
+
+export default databaseConfig
