6ba5cb4001d45475066a43e7401a581b350b6317
James <lanzonprojects@gmail.com>
2023-05-14 23:07:24 +0200

    Creating routes
    - Created the controllers to function as API only (not serve any server side forms).
    - Registered routes.

--- api-new/start/routes.ts
+++ api-new/start/routes.ts
@@ -23,3 +23,18 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.group(() => {
+  Route.resource('profile', 'ProfilesController')
+    // .middleware({ '*': ['auth'] })
+    .apiOnly()
+  Route.resource('sites', 'SitesController')
+    // .middleware({ '*': ['auth'] })
+    .apiOnly()
+  Route.resource('meters', 'MetersController')
+    // .middleware({ '*': ['auth'] })
+    .apiOnly()
+  Route.resource('circuits', 'CircuitsController')
+    // .middleware({ '*': ['auth'] })
+    .apiOnly()
+})
