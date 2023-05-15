e4b1a53eddd3421b02d3b61192a75d94c2b1bd17
Koshux <lanzonprojects@gmail.com>
2023-05-16 00:37:17 +0200

    Turned off preload
    Turned off preload of relationship data to resume with frontend.

--- app/Controllers/http/ProfileController.ts
+++ app/Controllers/http/ProfileController.ts
@@ -8,17 +8,16 @@ export default class ProfileController {
   }
 
   public async show({ }: HttpContextContract) {
-    // return Customer.findOrFail(1)
+    return Customer.findOrFail(1)
 
     // TODO: test pre-load all relations.
-    return Customer.query()
-      .preload('sites', (query) => {
-        query.preload('meters', (meterQuery) => {
-          meterQuery.preload('circuits')
-        })
-      })
-      .firstOrFail()
-
+    // return Customer.query()
+    //   .preload('sites', (query) => {
+    //     query.preload('meters', (meterQuery) => {
+    //       meterQuery.preload('circuits')
+    //     })
+    //   })
+    //   .firstOrFail()
   }
 
   public async store({ request, response }: HttpContextContract) {
